b8cc81860c3d9c087f177eb3088d5c4ff72d40d1
ArefehPeimani <arefehpeimani@gmail.com>
2023-04-19 22:30:09 +0330

    Completed Lecture 5 Exercises

--- src/main/java/Lecture5Exercises.java
+++ src/main/java/Lecture5Exercises.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Lecture5Exercises {
 
     /*
@@ -6,7 +8,12 @@ public class Lecture5Exercises {
      *   lecture 5 page 14
      */
     public String weakPassword(int length) {
-        return null;
+        Random rand = new Random();
+        String res = "";
+        for (int i = 0; i < length; i++) {
+            res += (char)(rand.nextInt(26) + 97);
+        }
+        return res;
     }
 
     /*
@@ -15,7 +22,33 @@ public class Lecture5Exercises {
      *   lecture 5 page 14
      */
     public String strongPassword(int length) throws Exception {
-        return null;
+        if (length < 3) {
+            throw new Exception("Length must be more than 3");
+        }
+        Random rand = new Random();
+        String res = "";
+        res += (char)(rand.nextInt(9)+49);
+        res += (char)(rand.nextInt(15)+33);
+        res += weakPassword(length-2);
+        int newIndex = rand.nextInt(length);
+        if (newIndex != 0 && newIndex != 1) {
+            res = swap(res, 0, newIndex);
+        }
+        newIndex = rand.nextInt(length);
+        if (newIndex != 0 && newIndex != 1) {
+            res = swap(res, 1, newIndex);
+        }
+        return res;
+    }
+
+    public String swap(String str, int i, int j)
+    {
+        if (j == str.length() - 1) {
+            return str.substring(0, i) + str.charAt(j) + str.substring(i + 1, j) + str.charAt(i);
+        }
+        else {
+            return str.substring(0, i) + str.charAt(j) + str.substring(i + 1, j) + str.charAt(i) + str.substring(j + 1);
+        }
     }
 
     /*
@@ -27,6 +60,19 @@ public class Lecture5Exercises {
      *   lecture 5 page 17
      */
     public boolean isFiboBin(int n) {
+        for (int i = 1; i < n; i++) {
+            long fib = fibonacci(i);
+            if (fib + Long.bitCount(fib) == n) {
+                return true;
+            }
+        }
         return false;
+    }
+
+    public long fibonacci(int n) {
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
     }
 }
